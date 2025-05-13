16436ea749aeb60fa0440bda2622bfd1c205b675
vijay kumar, samayamantula <vijay@sparc.science>
2025-05-13 17:02:59 -0400

    upgrade template app to vue3

--- src/main.js
+++ src/main.js
@@ -1,20 +1,7 @@
-import Vue from 'vue'
-import App from './App'
-import Vuex from 'vuex'
-import SystemDesignComponents from '@nih-sparc/sparc-design-system-components'
-import * as svgicon from 'vue-svgicon'
+import { createApp } from 'vue'
+import App from './App.vue'
+import store from './store'
 
-
-Vue.use(Vuex)
-Vue.use(SystemDesignComponents)
-Vue.use(svgicon, {
-  tagName: 'svg-icon'
-})
-
-Vue.config.productionTip = false
-
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
-})
+const app = createApp(App)
+app.mount('#app')
+app.use(store)
