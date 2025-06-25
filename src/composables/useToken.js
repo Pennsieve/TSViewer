99e48b984f9f09f0d08490676043f4fe1c20b482
vijay kumar, samayamantula <vijay@sparc.science>
2025-06-25 14:50:48 -0400

    update config for nuxt app

--- src/composables/useToken.js
+++ src/composables/useToken.js
@@ -1,9 +1,20 @@
-import { inject } from 'vue';
+import { inject, getCurrentInstance } from 'vue';
+import { useNuxtApp } from 'nuxt/app';
 
 export function useToken() {
-  const useGetToken = inject('useGetToken');
-  if (!useGetToken) {
-    throw new Error('useGetToken is not provided. Make sure to provide it in your app.');
+  const nuxtApp = getCurrentInstance() ? null : useNuxtApp();
+
+  if (nuxtApp) {
+    const getToken = nuxtApp.$getToken;
+    if (!getToken) {
+      throw new Error('getToken is not provided via Nuxt app.');
+    }
+    return getToken;
+  } else {
+    const useGetToken = inject('useGetToken');
+    if (!useGetToken) {
+      throw new Error('useGetToken is not provided via Vue inject.');
+    }
+    return useGetToken;
   }
-  return useGetToken;
 }
