e2640b69a02e77b03ca8ceba387f673f1c93e8b3
vijay kumar, samayamantula <vijay@sparc.science>
2025-06-30 16:08:19 -0400

    bump version and fix authentication

--- src/composables/useToken.js
+++ src/composables/useToken.js
@@ -3,8 +3,6 @@ import {fetchAuthSession} from "aws-amplify/auth";
 export async function useToken() {
     try {
         return fetchAuthSession().then((session) => {
-            console.log('session object', session)
-            console.log('access token is', session?.tokens?.accessToken.toString())
             return session?.tokens?.accessToken.toString();
             }
         );
